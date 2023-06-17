f7bb2e8979e3c4343cc85ad897c355c729fdfcf1
ruanzindeluxe <128759460+ruanzinn7b@users.noreply.github.com>
2023-06-16 21:40:16 -0300

    Update alterarsenha.php

--- alterarsenha.php
+++ alterarsenha.php
@@ -1,71 +1,80 @@
-<!DOCTYPE html>
+<?php
+session_start();
+
+
+if (!isset($_SESSION['nome'])) {
+    header("Location: login.php"); 
+    exit();
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  
+    $conexao = mysqli_connect('localhost', 'root', '', 'bancoa3','3306');
+
+    
+    if (!$conexao) {
+        die("Falha na conexão com o banco de dados: " . mysqli_connect_error());
+    }
+
+    
+    $novasenha = $_POST['nova_senha'];
+    $confirmarsenha = $_POST['confirmar_senha'];
+
+    
+    if ($novasenha !== $confirmarsenha) {
+        die("A nova senha e a confirmação da senha não correspondem.");
+    }
+
+    
+    $nomeUsuario = $_SESSION['nome']; 
+    $senhaHash = $novasenha;
+
+    $sql = "UPDATE login SET senha = '$senhaHash' WHERE nome = '$nomeUsuario'";
+    if (mysqli_query($conexao, $sql)) {?>
+        Senha atualizada com sucesso
+        <br><br>
+        <a href="login.php">Voltar para a página de login</a>
+  <?php  } else {
+        echo "Erro ao atualizar a senha: " . mysqli_error($conexao);
+    }
+
+    // Feche a conexão com o banco de dados
+    mysqli_close($conexao);
+}
+?>
+
 <html>
-<head>
-    <title>Alterar Senha</title>
-    <style>
-        body {
-            background-image: url('https://i.pinimg.com/originals/11/94/58/11945894609c1f8841ab1aba0a1da21d.jpg');
-            background-size: cover;
-            background-position: center;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
-
-        form {
-            background-color: #000000; /* Altere para a cor preta desejada */
-            padding: 20px;
-            border-radius: 5px;
-        }
-
-        label,
-        input[type="password"] {
-            display: block;
-            padding: 10px;
-            border: none;
-            border-radius: 5px;
-            margin-bottom: 10px;
-        }
-
-        input[type="submit"] {
-            padding: 10px 20px;
-            background-color: #4CAF50;
-            color: white;
-            border: none;
-            cursor: pointer;
-        }
-
-        input[type="submit"]:hover {
-            background-color: #45a049;
-        }
-        
-        .error {
-            color: red;
-            margin-bottom: 10px;
-        }
-        .nova{
-            color:white;
-        }
+<style>
+      body{
+        font-family: Arial, Helvetica, sans-serif;
+        background-image: linear-gradient(45deg,black, white);
+      }
+      div{
+        background-color: rgba(0, 0, 0,0.9);
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        padding: 110px;
+        border-radius: 10px;
+        color: white;
+      }
+       
+      
     </style>
-</head>
 <body>
-    <center>
-    <h1 class="nova" >Alterar Senha</h1>
-    <form method="POST" action="senhaatualizada.php">
-        <label for="nova_senha" class="nova">Nova senha:</label>
-        <input type="password" name="nova_senha" id="nova_senha" required>
+<div>
+   <!-- Exiba o formulário de alteração de senha -->
+   <h1>Alterar Senha</h1>
+   <form method="POST" action="alterarsenha.php">
+       <label for="nova_senha">Nova senha:</label>
+       <input type="password" name="nova_senha" id="nova_senha" required>
 
-        <label for="confirmar_senha" class="nova">Confirmar nova senha:</label>
-        <input type="password" name="confirmar_senha" id="confirmar_senha" required>
+       <label for="confirmar_senha"><br><br>
+        Confirmar nova senha:</label> <input type="password" name="confirmar_senha" id="confirmar_senha" required><br><br>
 
-        <input type="submit" value="Alterar Senha">
-        
-        <?php if (isset($erro)) { ?>
-        <div class="error"><?php echo $erro; ?></div>
-        <?php } ?>
-    </form>
-    </center>
+       <input type="submit" value="Alterar Senha">
+   </form>
+    </div>
 </body>
 </html>
-
